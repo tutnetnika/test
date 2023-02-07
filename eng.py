5d303b47d2b91327053b6b8d688e0bc79c44d4ea
tutnetnika <81846353+tutnetnika@users.noreply.github.com>
2023-02-07 23:48:07 +0300

    Update eng.py

--- eng.py
+++ eng.py
@@ -4,16 +4,11 @@ correct_ans = 0
 tries = 3
 total_questions = len(questions)
 
-def print_tries(tries):
-    if tries == 2:
-        print(f"Неправильно.\nОсталось попыток: {tries}, попробуйте еще раз!")
-        user_input = input()
-    elif tries == 1:
-        print(f"Неправильно.\nОсталось попыток: {tries}, попробуйте еще раз!")
-        user_input = input()
-    else:
+def print_tries(try_n):
+    if try_n == tries - 1:
         print(f"Увы, но нет. Верный ответ: {answers[i]}")
-        user_input = input()
+    else:
+        print(f"Неправильно.\nОсталось попыток: { tries - try_n - 1}, попробуйте еще раз!")
 
 # Приглашение на игру
 print("Привет!\nПредлагаю проверить свои знания английского!\nНаберите \"ready\", чтобы начать!")
@@ -21,15 +16,15 @@ user_input = input()
 if user_input == "ready":
     for i in range(len(questions)):
         print(questions[i])
-        for tries in range(3):
+        for try_n in range(tries):
             user_input = input()
             if user_input == answers[i]:
                 print("ответ верный")
                 correct_ans += 1
                 break
-        else:
-            tries -= 1
-            print_tries(tries)
+            else:
+                print_tries(try_n)
+                
 else:
     print("Кажется, вы не хотите играть. Очень жаль.")
 
